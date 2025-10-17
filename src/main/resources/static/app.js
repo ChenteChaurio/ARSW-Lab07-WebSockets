44fe96cd9586d72d9f781d551b2f82c449df4edf
ChenteChaurio <vicentegarzonrios@gmail.com>
2025-10-17 01:07:35 -0500

    PARTE 3

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -8,13 +8,13 @@ var app = (function () {
     }
 
     var stompClient = null;
+    var drawingId = null;
 
     var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
-        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
-        ctx.fillStyle = "blue";
+        ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
         ctx.fill();
     };
 
@@ -31,9 +31,12 @@ var app = (function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
+
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+            console.log('Subscribed to /topic/newpoint.' + drawingId);
+
+            stompClient.subscribe('/topic/newpoint.' + drawingId, function (eventbody) {
                 var point = JSON.parse(eventbody.body);
                 addPointToCanvas(point);
             });
@@ -41,20 +44,35 @@ var app = (function () {
     };
 
     return {
-
         init: function () {
             var can = document.getElementById("canvas");
+
+            // Captura clics en el canvas
             can.addEventListener("click", function (evt) {
-                var point = getMousePosition(evt);
-                app.publishPoint(point.x, point.y);
+                if (stompClient && drawingId) {
+                    var point = getMousePosition(evt);
+                    app.publishPoint(point.x, point.y);
+                } else {
+                    alert("Primero debes conectarte a un dibujo.");
+                }
             });
+        },
+
+        connect: function () {
+            var id = document.getElementById("drawingId").value;
+            if (!id) {
+                alert("Por favor ingresa un ID de dibujo antes de conectarte.");
+                return;
+            }
+            drawingId = id;
             connectAndSubscribe();
         },
-        publishPoint: function(px, py){
+
+        publishPoint: function (px, py) {
             var pt = new Point(px, py);
-            console.info("publishing point at (" + pt.x + ", " + pt.y + ")");
+            console.info("Publishing point at (" + pt.x + ", " + pt.y + ") to drawing " + drawingId);
             addPointToCanvas(pt);
-            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
+            stompClient.send("/app/newpoint." + drawingId, {}, JSON.stringify(pt));
         },
 
         disconnect: function () {
@@ -64,5 +82,4 @@ var app = (function () {
             console.log("Disconnected");
         }
     };
-
 })();
